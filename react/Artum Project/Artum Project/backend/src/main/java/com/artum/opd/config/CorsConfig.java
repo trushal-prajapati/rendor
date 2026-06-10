4df5116a266994d90965a4bc345ea7acb10a942b
trushal-prajapati <tprajapati2908@gmail.com>
2026-06-10 16:41:44 +0530

    artum corigins

--- react/Artum Project/Artum Project/backend/src/main/java/com/artum/opd/config/CorsConfig.java
+++ react/Artum Project/Artum Project/backend/src/main/java/com/artum/opd/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://rendor-opq8.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://rendor-opq8.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
